7c28d699090b24b75e61bd345c78da81e955d7e6
Xbot-me <33694482+Xbot-me@users.noreply.github.com>
2018-09-25 05:34:59 +0600

    Search Done

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,30 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-search.getSearchResult();
-console.log(search);
+import * as searchView from './views/searchView';
+
+import { elements } from './views/base';
+
+// Global state
+const state = {};
+
+const controlSearch = async () => {
+    const query = searchView.getInput();
+    
+    if(query) {
+        state.search = new Search(query);
+        await state.search.getSearchResult();
+        searchView.clearResult();
+        searchView.clearInput();
+        searchView.renderResults(state.search.result);
+
+    }
+}
+
+
+ elements.searchForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    controlSearch();
+} );
+
+
+ 
